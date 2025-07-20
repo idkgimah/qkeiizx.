08a2a12920f30a5b149868817d5dca4ec49b860e
idkgimah <f25687883@gmail.com>
2025-07-20 01:23:52 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/eggrefresh/eggesp/refs/heads/main/Loader.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/eggrefresh/eggesp/refs/heads/main/Loader.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/nlWMuvroN3/0", true))()
